7181d6190b172298bd753a7feafbf2656961924b
Arman Hossain <web.devarman@gmail.com>
2026-03-16 05:53:32 +0600

    fixed cors and prisma.ts for deploy

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -10,7 +10,10 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({
+  origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
+  credentials: true
+}));
 
 // application routes
 app.use('/api', router);
--- Backend/src/lib/prisma.ts
+++ Backend/src/lib/prisma.ts
@@ -2,10 +2,11 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
+const connectionString = process.env.DATABASE_URL as string;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+export const prisma =
+    globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
