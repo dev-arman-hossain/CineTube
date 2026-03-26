bb5fdfeba95ccf01ae0b75426c980a611b61e540
Arman Hossain <web.devarman@gmail.com>
2026-03-27 04:30:31 +0600

    Fetch profile image from google servers

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
         hostname: 'i.pinimg.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
     ],
   },
 };
